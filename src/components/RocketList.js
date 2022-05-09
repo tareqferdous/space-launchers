02ef7b5dae8fe7a56fe64466b8c5a039d0e4cab3
Tareq Ferdous <16101017@uap-bd.edu>
2022-05-09 23:10:37 +0600

    loading state handle

--- src/components/RocketList.js
+++ src/components/RocketList.js
@@ -12,6 +12,18 @@ const RocketList = ({ rocket }) => {
     dispatch(getRockets());
   }, []);
 
+  if (loading) {
+    return (
+      <div class="flex items-center justify-center min-h-screen p-5 bg-gray-100 min-w-screen">
+        <div class="flex space-x-2 animate-pulse">
+          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+          <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-4 gap-4">
       {rockets
